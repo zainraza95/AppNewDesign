657ac9b4c00e79718ad102562fa5dc29a378434d
zain.raza <mzr955@outlook.com>
2025-01-02 00:18:05 +0500

    cache

--- wwwroot/js/clearcache.js
+++ wwwroot/js/clearcache.js
@@ -1,13 +1,13 @@
 ﻿
-    $('#clearCacheButton').on('click', function () {
-        $.ajax({
-            url: '/Cache/Clear',
-            type: 'POST',
-            success: function (response) {
-                alert('Cache cleared successfully!');
-            },
-            error: function (error) {
-                alert('Error clearing cache.');
-            }
-        });
+$('#clearCacheButton').on('click', function () {
+    $.ajax({
+        url: '/Cache/Clear',
+        type: 'POST',
+        success: function (response) {
+            alert('Cache cleared successfully!');
+        },
+        error: function (error) {
+            alert('Error clearing cache.');
+        }
     });
+});
